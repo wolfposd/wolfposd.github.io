b883ed663be1ac5881092c976666235efbf58639
wolf <wolf@Wolfs-Mac.local>
2015-09-30 11:59:37 +0200

    update

--- import.sh
+++ import.sh
@@ -4,3 +4,12 @@
 dpkg-scanpackages -m . /dev/null >Packages
 rm Packages.bz2
 bzip2 Packages
+
+
+
+echo "<html><body>HELLO!<br>Please add this address in Cydia:<br>wolfposd.github.io<br><br><br><br><br>Last Update " > index.html
+
+
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+echo $DATE >> index.html
+echo "</body></html>" >> index.html
