8cbc78fc52942d188e167bea5a370c9925f5d071
Ich <posdorfer@informatik.uni-hamburg.de>
2016-09-19 14:52:58 +0200

    new style test

--- import.sh
+++ import.sh
@@ -6,13 +6,11 @@ rm Packages.bz2
 bzip2 Packages
 
 
-
-echo "<html><body>HELLO!<br>Please add this address in Cydia:<br>http://wolfposd.github.io<br><br>Or <a href='cydia://url/https://cydia.saurik.com/api/share#?source=http://wolfposd.github.io'>Click Here</a><br><br><br>Last Update " > index.html
-
-
-DATE=`date +%Y-%m-%d_%H:%M:%S`
-echo $DATE >> index.html
-echo "</body></html>" >> index.html
+# updates the date:
+#echo "<html><body>HELLO!<br>Please add this address in Cydia:<br>http://wolfposd.github.io<br><br>Or <a href='cydia://url/https://cydia.saurik.com/api/share#?source=http://wolfposd.github.io'>Click Here</a><br><br><br>Last Update " > index.html
+#DATE=`date +%Y-%m-%d_%H:%M:%S`
+#echo $DATE >> index.html
+#echo "</body></html>" >> index.html
 
 
 git add *
