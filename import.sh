f157b17b5acaeccb759e95afd7dd84776525e0d3
Ich <posdorfer@informatik.uni-hamburg.de>
2016-11-21 18:01:06 +0100

    udpate

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #dpkg-scanpackages2 ./ /dev/null |gzip > Packages.gz
 
-dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m ./deb/ /dev/null >Packages
 rm Packages.bz2
 bzip2 Packages
 
