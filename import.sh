90d1aea36f418d69b8a21129df5e8b63aa78016e
Ich <posdorfer@informatik.uni-hamburg.de>
2016-11-22 12:17:36 +0100

    udpate 2016-11-22_12:17:36

--- import.sh
+++ import.sh
@@ -4,11 +4,11 @@
 dpkg-scanpackages -m ./deb/ /dev/null >Packages
 rm Packages.bz2
 bzip2 Packages
-
+rm Packages
 
 # updates the date:
 #echo "<html><body>HELLO!<br>Please add this address in Cydia:<br>http://wolfposd.github.io<br><br>Or <a href='cydia://url/https://cydia.saurik.com/api/share#?source=http://wolfposd.github.io'>Click Here</a><br><br><br>Last Update " > index.html
-#DATE=`date +%Y-%m-%d_%H:%M:%S`
+DATE=`date +%Y-%m-%d_%H:%M:%S`
 #echo $DATE >> index.html
 #echo "</body></html>" >> index.html
 
